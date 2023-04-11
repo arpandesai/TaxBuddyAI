c9e5a46da81bc7dbb8de672098a5bbe4baf2a7df
Jaydeep Patel <58411819+jaydeep-patel-au6@users.noreply.github.com>
2023-04-11 12:24:30 +0530

    Update guidline.tsx

--- src/data/guidline.tsx
+++ src/data/guidline.tsx
@@ -2,7 +2,7 @@ export const guidlines = [
   {
     text: 'Ask clear questions',
     subText:
-      'When asking a question, try to be as specific and clear as possible. The model is trained on a wide range of topics, but the more information you provide, the better the model can respond.',
+      'When asking a question, try to be as specific and clear as possible. TaxBuddyAi is trained on a income tax topics, but the more information you provide, the better the TaxBuddyAi can respond.',
   },
   {
     text: 'Please use numbers instead of words in TaxBuddyAI.',
@@ -11,26 +11,25 @@ export const guidlines = [
   {
     text: 'Use proper grammar and spelling',
     subText:
-      'Good grammar and spelling help the model better understand your question and provide a clearer response.',
+      'Good grammar and spelling help the TaxBuddyAi better understand your question and provide a clearer response.',
   },
   {
     text: 'Be Specific',
     subText:
-      'The more specific your questions, the more accurate ChatGPT’s answers will be. So, try to avoid asking vague questions',
+      'The more specific your questions, the more accurate TaxBuddyAi’s answers will be. So, try to avoid asking vague questions',
   },
   {
     text: 'Be patient',
     subText:
-      'The model may take a few moments to generate a response, especially for complex questions.',
-  },
-  {
-    text: 'Provide context',
-    subText:
-      'If your question is related to a specific topic or situation, provide some background information to help the model better understand what you’re looking for.',
+      'The TaxBuddyAi may take a few moments to generate a response, especially for complex questions.',
   },
   {
     text: 'Rephrase Questions',
     subText:
       'If you don’t get the answer you’re looking for, try rephrasing your question or providing more information.',
   },
+  {
+    text: 'TaxBuddyAi does not respond',
+    subText: 'Because of heavy traffic. sometimes TaxBuddyAI takes a long time to respond. just refresh the tab and TaxBuddyAi will work fine.'
+  }
 ];
